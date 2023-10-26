3dbab9154d0837e5e07c59121f477e35763bb06a
Khaniee <roseiloharaoke@gmail.com>
2023-10-26 21:28:25 +0300

    khah

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my_project/main.dart';
+import 'package:my_project/main_old.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
