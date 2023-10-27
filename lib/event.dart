8123b6518ad598ffebf4b33841c320024f602181
Khaniee <roseiloharaoke@gmail.com>
2023-10-27 09:59:31 +0300

    fonctionnality add event

---
+++ lib/event.dart
@@ -0,0 +1,16 @@
+class Event {
+  final String title;
+  // final DateTime date_debut;
+  // final DateTime date_fin;
+  // final String type;
+  final String lieu;
+  // final String choses_apporter;
+  Event(
+    this.title,
+    // this.date_debut,
+    // this.date_fin,
+    // this.type,
+    this.lieu,
+    // this.choses_apporter,
+  );
+}
