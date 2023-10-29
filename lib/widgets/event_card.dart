ae21d9d53f31bbc4f566bd167693ca96e71233a7
Lovanirina R <lovanirina.ran@gmail.com>
2023-10-29 13:02:46 +0300

    add image

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -29,138 +29,157 @@ class EventCard extends StatelessWidget {
         color: AppColor.white,
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Column(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              AppText(
-                event.title,
-                isBold: true,
-              ),
-              const Expanded(child: SizedBox()),
-              AppText(
-                  "${DateFormat('HH:mm').format(event.dateDebut)} - ${DateFormat('HH:mm').format(event.dateFin)}"),
-            ],
+          Container(
+            width: 90,
+            height: 90,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.only(right: 15),
+            decoration: BoxDecoration(
+              color: AppColor.lightPrimary.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image(image: AssetImage("assets/${event.type}.png")),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 5,
-                ),
-                decoration: BoxDecoration(
-                  color: AppColor.primary.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                    child: AppText(
-                  event.type,
-                  color: AppColor.primary,
-                  fontSize: AppFontSize.small,
-                  isBold: true,
-                )),
-              ),
-              if (event.chosesApporter != null &&
-                  event.chosesApporter!.isNotEmpty) ...[
-                const SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          Expanded(
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    const AppText(
-                      "N'oubliez pas",
-                      fontSize: AppFontSize.small,
+                    AppText(
+                      event.title,
                       isBold: true,
                     ),
+                    const Expanded(child: SizedBox()),
                     AppText(
-                      event.chosesApporter!,
-                      fontSize: AppFontSize.small,
+                        "${DateFormat('HH:mm').format(event.dateDebut)} - ${DateFormat('HH:mm').format(event.dateFin)}"),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColor.primary.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                          child: AppText(
+                        event.type,
+                        color: AppColor.primary,
+                        fontSize: AppFontSize.small,
+                        isBold: true,
+                      )),
                     ),
+                    if (event.chosesApporter != null &&
+                        event.chosesApporter!.isNotEmpty) ...[
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const AppText(
+                            "N'oubliez pas",
+                            fontSize: AppFontSize.small,
+                            isBold: true,
+                          ),
+                          AppText(
+                            event.chosesApporter!,
+                            fontSize: AppFontSize.small,
+                          ),
+                        ],
+                      )
+                    ]
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Icon(
+                          Icons.location_on_sharp,
+                          color: AppColor.textColor,
+                          size: 16,
+                        ),
+                        AppText(
+                          event.lieu,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            showModalBottomSheet(
+                              isScrollControlled: true,
+                              backgroundColor: Colors.transparent,
+                              context: context,
+                              builder: (BuildContext context) {
+                                return EventForm(
+                                  event: event,
+                                  callback: onEventModify,
+                                );
+                              },
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.edit_note,
+                            color: AppColor.divider,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: const Text("Avertissement"),
+                                content: const AppText(
+                                    "Vous êtes sur de supprimer cet évènement?"),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("Annuler")),
+                                  TextButton(
+                                      onPressed: () {
+                                        if (event.id != null) {
+                                          EventService.deleteEvent(event.id!);
+                                          onEventModify();
+                                        }
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("Supprimer")),
+                                ],
+                              ),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.delete_sweep,
+                            color: AppColor.divider,
+                          ),
+                        )
+                      ],
+                    )
                   ],
                 )
-              ]
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Icon(
-                    Icons.location_on_sharp,
-                    color: AppColor.textColor,
-                    size: 16,
-                  ),
-                  AppText(
-                    event.lieu,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      showModalBottomSheet(
-                        isScrollControlled: true,
-                        backgroundColor: Colors.transparent,
-                        context: context,
-                        builder: (BuildContext context) {
-                          return EventForm(
-                            event: event,
-                            callback: onEventModify,
-                          );
-                        },
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.edit_note,
-                      color: AppColor.divider,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text("Avertissement"),
-                          content: const AppText(
-                              "Vous êtes sur de supprimer cet évènement?"),
-                          actions: [
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text("Annuler")),
-                            TextButton(
-                                onPressed: () {
-                                  if (event.id != null) {
-                                    EventService.deleteEvent(event.id!);
-                                    onEventModify();
-                                  }
-                                  Navigator.pop(context);
-                                },
-                                child: const Text("Supprimer")),
-                          ],
-                        ),
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.delete_sweep,
-                      color: AppColor.divider,
-                    ),
-                  )
-                ],
-              )
-            ],
+              ],
+            ),
           )
         ],
       ),
